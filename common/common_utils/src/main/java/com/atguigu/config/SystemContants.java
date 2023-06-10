e71e80e4c44b6530364ffa73a7a5ca785d132938
jasper <shamsjasper@gmail.com>
2023-06-10 20:39:50 +0800

    final 管理

---
+++ common/common_utils/src/main/java/com/atguigu/config/SystemContants.java
@@ -0,0 +1,12 @@
+package com.atguigu.config;
+
+/**
+ * @author jasper
+ * @date 2023/5/14
+ */
+public class SystemContants {
+    public static final String ENDPOINT = "oss-cn-shanghai.aliyuncs.com";
+    public static final String ACCESSKEYID = "LTAI5tHJjeqMBPRPHJ7dYCdn";
+    public static final String ACCESSKEYSECRET ="RKXP0lIxBaUZJ0DUS0b56qL8uqSooM";
+    public static final String BUCKETNAME = "cliffbucket";
+}
